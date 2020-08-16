fa922cb55cd0daf0d0121897e1a33a940a101346
Minenhle <47856302+minenhle-qf@users.noreply.github.com>
2020-08-16 18:05:37 +0100

    Update Queue.py

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,8 +1,10 @@
 class Queue:
     
+    #Creating the queue
     def __init__(self):
         self.queue = list()
-        
+      
+    #Enqueue
     def addToQueue(self, data):
         if data not in self.queue:
             self.queue.insert(0 ,data)
@@ -10,11 +12,13 @@ class Queue:
         else:
             return False
     
+    #Dequeue
     def removeFromQueue(self):
         if len(self.queue) > 0:
             return self.queue.pop()
         return ("No elemenst in the queue")
     
+    #Determining the size of the queue
     def size(self):
         return len(self.queue)
     
